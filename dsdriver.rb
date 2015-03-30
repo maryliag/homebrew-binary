b80f66faff5791975c7f0ed8be8fb5a1436e6e16
Marylia Gutierrez <maryliag@gmail.com>
2015-03-30 15:53:45 -0400

    homepage

--- dsdriver.rb
+++ dsdriver.rb
@@ -1,7 +1,7 @@
 require "formula"
 
 class Dsdriver < Formula
-  homepage ""
+  homepage "http://www-01.ibm.com/support/knowledgecenter/SS6NHC/com.ibm.swg.im.dashdb.doc/connecting/connect_driver_package.html"
   url "http://bd81.http.dal05.cdn.softlayer.net/public/dsdriver/dsdriver-10.5fp5_33751.tar.gz"
   version "10.5.0.5.33751"
   sha1 "b0aca23b10eb7bb51d0c677fd5ea1ed5ec8481e9"
