2eb12d2f1ea1f2cef2f8946b85eaef62022442fa
Marylia Gutierrez <maryliag@gmail.com>
2015-03-30 15:50:13 -0400

    new dsdriver

---
+++ dsdriver.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+class Dsdriver < Formula
+  homepage ""
+  url "http://bd81.http.dal05.cdn.softlayer.net/public/dsdriver/dsdriver-10.5fp5_33751.tar.gz"
+  version "10.5.0.5.33751"
+  sha1 "b0aca23b10eb7bb51d0c677fd5ea1ed5ec8481e9"
+
+  def install
+  	if Dir.exists?('/Applications/dsdriver')
+  		puts "You already have dsdriver installed on /Applications/dsdriver."
+  		puts "Please, remove this folder and try again."
+  	else
+  		puts "Installing... "
+    	system "./installDSDriver", prefix
+    end
+  end
+end
